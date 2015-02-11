e1169e5620a92c16df286247dd9cd12cbb1fbbc2
King Ying <king.ying@gibson.com>
2015-02-11 18:05:49 +0800

    Initial Commit

---
+++ UICollectionViewContinuity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UICollectionViewContinuity
+//
+//  Created by King.Ying on 2/11/15.
+//  Copyright (c) 2015 YKYING.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
